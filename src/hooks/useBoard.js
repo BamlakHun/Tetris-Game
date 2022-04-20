ba7451e677312cdec2a476f792e5c964a3ac06fd
Bamlak <49519072+BamlakHun@users.noreply.github.com>
2022-04-20 12:17:26 +0200

    Piece of tetrominos available Tetris board, tetrominos, player where player is active on the board, previews and status of the game has been implemented. Basic functionality is completed.

--- src/hooks/useBoard.js
+++ src/hooks/useBoard.js
@@ -1,8 +1,25 @@
-import { useState } from "react";
-import { buildBoard } from "/src/utilities/Board";
+import { useState, useEffect } from "react";
+import { buildBoard, nextBoard } from "/src/utilities/Board";
 
-export const useBoard = ({ rows, columns }) => {
-  const [board] = useState(buildBoard({ rows, columns })); // calls the functions
+export const useBoard = ({
+  rows,
+  columns,
+  player,
+  resetPlayer,
+  addLinesCleared
+}) => {
+  const [board, setBoard] = useState(buildBoard({ rows, columns })); // calls the functions
+  // the effect catches the changes made
+  useEffect(() => {
+    setBoard((previousBoard) =>
+      nextBoard({
+        board: previousBoard,
+        player,
+        resetPlayer,
+        addLinesCleared
+      })
+    );
+  }, [player, resetPlayer, addLinesCleared]);
 
   return [board];
 };
--- src/utilities/Board.js
+++ src/utilities/Board.js
@@ -1,13 +1,38 @@
 import { defaultCell } from "/src/utilities/Cell";
+import { transferToBoard } from "/src/utilities/Tetrominoes";
 
 export const buildBoard = ({ rows, columns }) => {
   // take the rows ans columns
   const builtRows = Array.from({ length: rows }, () =>
-    Array.from({ length: columns }, () => ({ defaultCell }))
+    Array.from({ length: columns }, () => ({ ...defaultCell }))
   );
 
   return {
     rows: builtRows,
     size: { rows, columns }
+  };
+};
+//  exportb functions
+export const nextBoard = ({ board, player, resetPlayer, addLinesCleared }) => {
+  const { tetromino, position } = player;
+
+  // Copy and clear spaces used by pieces that
+  // hadn't collided and occupied spaces permanently
+  // if it is occupied keep it, we only require to clear it
+  let rows = board.rows.map((row) =>
+    row.map((cell) => (cell.occupied ? cell : { ...defaultCell }))
+  );
+  rows = transferToBoard({
+    className: tetromino.className,
+    isOccupied: player.isFastDropping,
+    position,
+    rows,
+    shape: tetromino.shape
+  });
+
+  // Return the next board
+  return {
+    rows,
+    size: { ...board.size }
   };
 };
